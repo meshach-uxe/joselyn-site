89bbb4430e66ec32b3e4d88c5fd30689e689dd4a
meshach-uxe <meshachmacwright@gmail.com>
2026-03-27 09:37:14 +0000

    Remove search and user icons from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,17 +13,7 @@ const KindnessLogo = () => (
   />
 );
 
-const SearchIcon = () => (
-  <svg viewBox="0 0 24 24" width="22" height="22" fill="currentColor">
-    <path d="M13 5c-3.3 0-6 2.7-6 6 0 1.4.5 2.7 1.3 3.7l-3.8 3.8 1.1 1.1 3.8-3.8c1 .8 2.3 1.3 3.7 1.3 3.3 0 6-2.7 6-6S16.3 5 13 5zm0 10.5c-2.5 0-4.5-2-4.5-4.5s2-4.5 4.5-4.5 4.5 2 4.5 4.5-2 4.5-4.5 4.5z"/>
-  </svg>
-);
 
-const UserIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="-4 -4 25 25" width="22" height="22" fill="currentColor">
-    <path d="M9 0C4.03579 0 0 4.03579 0 9C0 13.9642 4.03579 18 9 18C13.9642 18 18 13.9642 18 9C18 4.03579 13.9642 0 9 0ZM9 4.32C10.5347 4.32 11.7664 5.57056 11.7664 7.08638C11.7664 8.62109 10.5158 9.85277 9 9.85277C7.4653 9.85277 6.23362 8.60221 6.23362 7.08638C6.23362 5.57056 7.46526 4.32 9 4.32ZM9 10.7242C11.1221 10.7242 12.96 12.2021 13.7937 14.4189C12.5242 15.5559 10.8379 16.238 9 16.238C7.16207 16.238 5.49474 15.5369 4.20632 14.4189C5.05891 12.2021 6.87793 10.7242 9 10.7242Z"/>
-  </svg>
-);
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -71,12 +61,7 @@ export default function Header() {
 
           {/* Right actions */}
           <div className={styles.headerActions}>
-            <button className={styles.iconBtn} aria-label="Search">
-              <SearchIcon />
-            </button>
-            <a href="#" className={styles.iconBtn} aria-label="Login">
-              <UserIcon />
-            </a>
+
             <a href="#" className="btn btn-sm">Donate</a>
 
             {/* Hamburger */}
